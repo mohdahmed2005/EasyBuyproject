ff04f05f976fb73254e5a4e34ca393192dec55fb
my_project-workspace-admin <syedsameeuddin123@gmail.com>
2025-07-27 16:46:59 +0530

    ghghghg

--- EasyBuy-Backend/src/main/java/com/main/ecommerce/service/ProductService.java
+++ EasyBuy-Backend/src/main/java/com/main/ecommerce/service/ProductService.java
@@ -30,7 +30,7 @@ public class ProductService {
     private CartDao cartDao;
 
     public Product addNewProduct(Product product) {
-    	System.out.println("updated from other repo");
+    	System.out.println("updated from other repo1");
         return productDao.save(product);
     }
 
